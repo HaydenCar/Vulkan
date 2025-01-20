fbbffaac43837b62df08ed99e073cfa4cb09b944
hayden <haydencarrbusiness@gmail.com>
2025-01-20 21:09:11 +0000

    git commit

--- vulkan/src/window.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef WINDOW_HPP
-#define WINDOW_HPP
-#include <GLFW/glfw3.h>
-
-namespace Window {
-    void initWindow(GLFWwindow* &window, uint32_t WIDTH, uint32_t HEIGHT);
-}
-
-#endif // WINDOW_HPP
